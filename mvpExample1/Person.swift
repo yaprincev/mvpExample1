3194f4e094e2c15d33aae300df9caac5636292a6
yaprincev <5489812@mail.ru>
2023-08-22 16:32:34 +0300

    init

---
+++ mvpExample1/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  mvpExample1
+//
+//  Created by Максим Япринцев on 22.08.2023.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
---
+++ mvpExample1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvpExample1
+//
+//  Created by Максим Япринцев on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
